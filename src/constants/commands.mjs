bf295af3690eade3604cfc1e1aa95bf5575f3f9d
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-14 15:44:31 +0200

    feat: implement os handler

--- src/constants/commands.mjs
+++ src/constants/commands.mjs
@@ -1,5 +1,6 @@
 import { handleLsCommand, handleCdCommand, handleUpCommand, handleDefaultCommand, handleExitCommand } from '../handlers/commandsHandlers.mjs';
 import { readFile, createFile, deleteFile, renameFile, copyFile, moveFile } from '../handlers/fileHandlers.mjs';
+import { getArchitecture, getCpus, getEol, getHomeDir, getSystemUserName } from '../handlers/osHandlers.mjs';
 
 export const commandsMap = {
     ls: handleLsCommand,
@@ -11,6 +12,11 @@ export const commandsMap = {
     rn: renameFile,
     cp: copyFile,
     mv: moveFile,
+    '--EOL': getEol,
+    '--cpus': getCpus,
+    '--homedir': getHomeDir,
+    '--username': getSystemUserName,
+    '--architecture': getArchitecture,
     '.exit': handleExitCommand,
     default: handleDefaultCommand,
 };
--- src/helpers/cliHandler.mjs
+++ src/helpers/cliHandler.mjs
@@ -3,9 +3,14 @@ import { handleExitCommand } from '../handlers/commandsHandlers.mjs';
 
 export const subscribe = () => {
     process.stdin.on('data', (data) => {
+        let handler;
         const inputValue = `${data}`.trim();
-        const command = inputValue.split(" ")[0];
-        const handler = commandsMap[command] || commandsMap.default;
+        const [primaryCommand, secondaryCommand] = inputValue.split(" ");
+        if (primaryCommand === 'os') {
+            handler = commandsMap[secondaryCommand] || commandsMap.default;
+        } else {
+            handler = commandsMap[primaryCommand] || commandsMap.default;
+        }
 
         handler(inputValue);
     });
